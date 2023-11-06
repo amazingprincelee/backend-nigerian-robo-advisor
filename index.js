7939937d5f180d03c7fba14b4d83e976add6acd5
D'Amazing Prince Lee <prince_lee.nna85@ymail.com>
2023-11-06 14:18:50 +0100

    update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const port = 3000
-const app = express()  
+const localPort = 3000;
+const app = express();  
 
 mongoose.connect(`mongodb+srv://amazingprincelee:${process.env.ATAFRICA_DB_PASSWORD}@cluster0.uumprvv.mongodb.net/atAfricaDB`)
   .then(() => console.log('Connected!'));
 
-app.use(express.json()) 
+app.use(express.json()); 
 
 const portfolioSchema = new mongoose.Schema({
     rickScore: Number,
@@ -22,37 +22,38 @@ const portfolioSchema = new mongoose.Schema({
     realEstate: String,
     tBills: String,
     alternative: String
-})
+});
 
 const Portfolio = mongoose.model('Portfolio', portfolioSchema, 'samplePortfolio');
 
 app.get("/api/portfolio", async (req, res)=>{
 
     try{
-        const data = await Portfolio.find();
+        const data = await Portfolio.find()
         res.json(data)
 
     }catch(err){
         res.status(500).json({message: "server error"})
     }
 
-})
+});
 
 app.get("/api/portfolio/:score", async (req, res)=>{
 
     try{
         const score = parseInt(req.params.score);
-        const data = await Portfolio.findOne({"Risk Score/Tolerence":score});
-        res.json(data);
+        const data = await Portfolio.findOne({"Risk Score/Tolerence":score})
+        res.json(data)
     }catch(error){
-        res.json({message:"no data found"});
+        res.json({message:"no data found"})
     }
     
     
-})
+});
 
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-})
+
+app.listen(process.env.PORT || localPort, ()=>{
+    console.log(`Server running on port ${localPort}`)
+});
 
